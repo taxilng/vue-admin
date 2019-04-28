125e025ea2c5fef91ec9d928d52a594cf09ca30f
taxilng <7699674440@qq.com>
2019-04-28 15:54:12 +0800

    适配移动端

--- src/components/tts/dialect/index.vue
+++ src/components/tts/dialect/index.vue
@@ -1,52 +1,3 @@
-<style lang="scss" scoped>
-.dialect {
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    li {
-      width: 140px;
-      height: 32px;
-      border: 1px solid #ccc;
-      margin: 10px;
-      padding: 5px 10px;
-    }
-  }
-  .listContent {
-    margin-top: 10px;
-    .left {
-      float: left;
-    }
-    .content {
-      width: 25%;
-      padding-bottom: 10px;
-      padding-right: 10px;
-      cursor: pointer;
-      .contentBox {
-        height: 32px;
-        border: 1px solid #dcdfe6;
-        font-size: 12px;
-        border-radius: 4px;
-        padding: 8px 15px 10px;
-        transition: all 0.3s;
-        position: relative;
-        img {
-          position: absolute;
-          right: 10px;
-          top: 6px;
-          width: 20px;
-          height: 20px;
-        }
-        .mdplay {
-          position: absolute;
-          right: 10px;
-          top: 9px;
-          font-size: 14px;
-        }
-      }
-    }
-  }
-}
-</style>
 
 <template>
   <div class="dialect">
@@ -58,27 +9,31 @@
       @playend="playend"
     ></player>
     <div class="listContent clear">
-      <div class="content left" v-for="(item, index) in soundData" :key="item.speakId">
-        <div
-          class="contentBox"
-          :style="{ borderColor: radio == item.speakId? '#4DA1FF': '#DCDFE6' }"
-          @click="checkBoxPick(index)"
-        >
-          <el-radio v-model="radio" :label="item.speakId">{{item.label}}</el-radio>
-          <img src="/static/runningVoice.gif" v-show="imgPk == item.speakId">
-          <i
-            class="el-icon-caret-right mdplay"
-            v-show="imgPk !== item.speakId"
-            @click="playSong(index)"
-          ></i>
-          <!-- <Icon
+      <el-row :gutter="10">
+        <el-col :xs="12" :sm="6" lass="content" v-for="(item, index) in soundData" :key="item.speakId">
+          <div class="content">
+          <div
+            class="contentBox"
+            :style="{ borderColor: radio == item.speakId? '#4DA1FF': '#DCDFE6' }"
+            @click="checkBoxPick(index)"
+          >
+            <el-radio v-model="radio" :label="item.speakId">{{item.label}}</el-radio>
+            <img src="/static/runningVoice.gif" v-show="imgPk == item.speakId">
+            <i
+              class="el-icon-caret-right mdplay"
+              v-show="imgPk !== item.speakId"
+              @click="playSong(index)"
+            ></i>
+            <!-- <Icon
             class="mdplay"
             type="md-play"
             v-show="imgPk !== item.speakId"
             @click="playSong(index)"
-          />-->
-        </div>
-      </div>
+            />-->
+          </div>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -135,3 +90,52 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+.dialect {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      width: 140px;
+      height: 32px;
+      border: 1px solid #ccc;
+      margin: 10px;
+      padding: 5px 10px;
+    }
+  }
+  .listContent {
+    margin-top: 10px;
+    .left {
+      float: left;
+    }
+    .content {
+      // width: 25%;
+      padding-bottom: 10px;
+      padding-right: 10px;
+      cursor: pointer;
+      .contentBox {
+        height: 32px;
+        border: 1px solid #dcdfe6;
+        font-size: 12px;
+        border-radius: 4px;
+        padding: 8px 15px 10px;
+        transition: all 0.3s;
+        position: relative;
+        img {
+          position: absolute;
+          right: 10px;
+          top: 6px;
+          width: 20px;
+          height: 20px;
+        }
+        .mdplay {
+          position: absolute;
+          right: 10px;
+          top: 9px;
+          font-size: 14px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/tts/index.vue
+++ src/views/tts/index.vue
@@ -1,10 +1,179 @@
+<template>
+  <div class="tts">
+    <player
+      :songUrl="songUrl"
+      :songVolume="songVolume"
+      :songRate="songRate"
+      :playing="playing"
+      @playend="playend"
+    ></player>
+    <el-row :gutter="10">
+      <el-col :xs="24" :sm="16">
+        <div class="content">
+          <div class="contentInput">
+            <el-input
+              type="textarea"
+              :rows="textareaRows"
+              placeholder="请输入内容"
+              v-model="textarea"
+              maxlength="100"
+            ></el-input>
+            <div class="counter">
+              (
+              <span class="textStyle">{{this.textarea.length}}</span>/100字)
+            </div>
+          </div>
+
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane
+              v-for="item in moduleList"
+              :key="item.name"
+              :label="item.label"
+              :name="item.name"
+            >
+              <dialect :songVolume="songVolume" :songRate="songRate"></dialect>
+            </el-tab-pane>
+          </el-tabs>
+          <el-row :gutter="20">
+            <el-col :xs="24" :sm="12">
+              <h3>音量</h3>
+              <el-slider v-model="volume" :format-tooltip="formatVolumeToolTip" class="slider"></el-slider>
+            </el-col>
+            <el-col :xs="24" :sm="12">
+              <h3>语速</h3>
+              <el-slider
+                v-model="speechRate"
+                :format-tooltip="formatSpeechRateToolTip"
+                :step="10"
+                show-stops
+                class="slider"
+              ></el-slider>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :xs="24" :sm="4">
+              <el-button style="min-width:100px;width:100%;" type="primary" @click="playSound()">
+                {{playText}}
+                <!-- <Icon :type="playIcon"/> -->
+              </el-button>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="8">
+        <div class="operating">
+          <h3>用法</h3>
+          <ul>
+            <li>
+              <span>数 字</span> : &nbsp;不需要加标点符号
+            </li>
+            <li>
+              <span>电话号码</span> : &nbsp;(#83278311)注意里面的「1」读作「幺」
+            </li>
+            <li>
+              <span>按位号码</span> : &nbsp;($32673)按数字读法「2」 读作「二」，「1」 读作「一」
+            </li>
+            <li>
+              <span>2两读法</span> : &nbsp;(@32673)按数字读法「2」 读作「两」
+            </li>
+            <li>
+              <span>2两读法</span> : &nbsp;(!32673)按数字读法「2」 读作「两」，「1」 读作「幺」
+            </li>
+            <li>
+              <span>注 意</span> : &nbsp;合成支持英文字母，数字，汉字组合
+            </li>
+          </ul>
+          <p></p>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import dialect from "@/components/tts/dialect";
+import player from "@/components/tts/dialect/audio";
+export default {
+  data() {
+    return {
+      songUrl: null,
+      songEnd: false,
+      textarea: "",
+      activeName: null,
+      speechRate: 50,
+      volume: 50,
+      playing: false,
+      moduleList: [
+        { name: "1", label: "模型1" },
+        { name: "2", label: "模型2" },
+        { name: "3", label: "模型3" },
+        { name: "4", label: "模型4" },
+        { name: "5", label: "模型5" },
+        { name: "6", label: "模型6" },
+        { name: "7", label: "模型7" },
+        { name: "8", label: "模型8" },
+        { name: "9", label: "模型9" }
+      ],
+      textareaRows: 15
+    };
+  },
+  computed: {
+    playIcon() {
+      return this.playing ? "md-pause" : "md-play";
+    },
+    playText() {
+      return this.playing ? "暂停播放" : "立即播放";
+    },
+    songVolume() {
+      return this.volume / 100;
+    },
+    songRate() {
+      return (this.speechRate + 50) / 100;
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.activeName = this.moduleList[0].name;
+    }, 100);
+    const winWidth = document.body.clientWidth 
+    if(winWidth < 768) {
+      this.textareaRows = 8
+    }
+    this.songUrl = "/static/szj.wav";
+  },
+  methods: {
+    playSound() {
+      this.playing = !this.playing;
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    formatVolumeToolTip(index) {
+      return "音量条: " + index;
+    },
+    formatSpeechRateToolTip(index) {
+      return "语速: " + (index + 50) / 100;
+    },
+    playend() {
+      this.playing = false;
+      this.songEnd = true;
+    }
+  },
+  watch: {},
+  components: {
+    dialect,
+    player
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .tts {
   margin: 20px;
-  display: flex;
+  // display: flex;
   .content {
-    flex: 3;
-    width: 600px;
+    // flex: 3;
+    // width: 600px;
     .contentInput {
       position: relative;
 
@@ -39,7 +208,7 @@
     }
   }
   .operating {
-    flex: 2;
+    // flex: 2;
     padding-left: 20px;
     h3 {
       font-weight: 700;
@@ -63,153 +232,3 @@
   }
 }
 </style>
-
-<template>
-  <div class="tts">
-    <player
-      :songUrl="songUrl"
-      :songVolume="songVolume"
-      :songRate="songRate"
-      :playing="playing"
-      @playend="playend"
-    ></player>
-    <div class="content">
-      <div class="contentInput">
-        <el-input type="textarea" rows="15" placeholder="请输入内容" v-model="textarea" maxlength="100"></el-input>
-        <div class="counter">
-          (
-          <span class="textStyle">{{this.textarea.length}}</span>/100字)
-        </div>
-      </div>
-
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane
-          v-for="item in moduleList"
-          :key="item.name"
-          :label="item.label"
-          :name="item.name"
-        >
-          <dialect :songVolume="songVolume" :songRate="songRate"></dialect>
-        </el-tab-pane>
-      </el-tabs>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <h3>音量</h3>
-          <el-slider v-model="volume" :format-tooltip="formatVolumeToolTip" class="slider"></el-slider>
-        </el-col>
-        <el-col :span="12">
-          <h3>语速</h3>
-          <el-slider
-            v-model="speechRate"
-            :format-tooltip="formatSpeechRateToolTip"
-            :step="10"
-            show-stops
-            class="slider"
-          ></el-slider>
-        </el-col>
-      </el-row>
-      <el-button type="primary" @click="playSound()">
-        {{playText}}
-        <!-- <Icon :type="playIcon"/> -->
-      </el-button>
-    </div>
-    <div class="operating">
-      <h3>用法</h3>
-      <ul>
-        <li>
-          <span>数 字</span> : &nbsp;不需要加标点符号
-        </li>
-        <li>
-          <span>电话号码</span> : &nbsp;(#83278311)注意里面的「1」读作「幺」
-        </li>
-        <li>
-          <span>按位号码</span> : &nbsp;($32673)按数字读法「2」 读作「二」，「1」 读作「一」
-        </li>
-        <li>
-          <span>2两读法</span> : &nbsp;(@32673)按数字读法「2」 读作「两」
-        </li>
-        <li>
-          <span>2两读法</span> : &nbsp;(!32673)按数字读法「2」 读作「两」，「1」 读作「幺」
-        </li>
-        <li>
-          <span>注 意</span> : &nbsp;合成支持英文字母，数字，汉字组合
-        </li>
-      </ul>
-      <p></p>
-    </div>
-  </div>
-</template>
-
-<script>
-import dialect from "@/components/tts/dialect";
-import player from "@/components/tts/dialect/audio";
-export default {
-  data() {
-    return {
-      songUrl: null,
-      songEnd: false,
-      textarea: "",
-      activeName: null,
-      speechRate: 50,
-      volume: 50,
-      playing: false,
-      moduleList: [
-        { name: "1", label: "模型1" },
-        { name: "2", label: "模型2" },
-        { name: "3", label: "模型3" },
-        { name: "4", label: "模型4" },
-        { name: "5", label: "模型5" },
-        { name: "6", label: "模型6" },
-        { name: "7", label: "模型7" },
-        { name: "8", label: "模型8" },
-        { name: "9", label: "模型9" }
-      ]
-    };
-  },
-  computed: {
-    playIcon() {
-      return this.playing ? "md-pause" : "md-play";
-    },
-    playText() {
-      return this.playing ? "暂停播放" : "立即播放";
-    },
-    songVolume() {
-      return this.volume / 100;
-    },
-    songRate() {
-      return (this.speechRate + 50) / 100;
-    }
-  },
-  mounted() {
-    setTimeout(() => {
-      this.activeName = this.moduleList[0].name;
-    }, 100);
-   this.songUrl = "/static/szj.wav";
-  },
-  methods: {
-    playSound() {
-      this.playing = !this.playing;
-    },
-    handleClick(tab, event) {
-      console.log(tab, event);
-    },
-    formatVolumeToolTip(index) {
-      return "音量条: " + index;
-    },
-    formatSpeechRateToolTip(index) {
-      return "语速: " + (index + 50) / 100;
-    },
-    playend() {
-      this.playing = false;
-      this.songEnd = true;
-    }
-  },
-  watch: {
-  },
-  components: {
-    dialect,
-    player
-  }
-};
-</script>
-
